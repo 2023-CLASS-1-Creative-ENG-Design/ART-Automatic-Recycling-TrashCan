423e10e00de48cd0183f6585234829c2b9a173eb
Minju Lee <mj0215l@naver.com>
2023-12-06 11:35:40 +0900

    1차 분류

---
+++ Arduino/First-classification.cpp
@@ -0,0 +1,80 @@
+#include <Arduino.h>
+#include <Servo.h>
+
+const int photoresistorPin = A0; // 조도센서를 연결한 아날로그 핀 번호
+const int threshold = 900; // 어두운 환경으로 간주할 임계값, 조절이 필요할 수 있습니다.
+
+void moveCan(); // 1차 분류 알고리즘
+void moveNonCan();
+void firstRecycling();
+
+
+Servo myservo1;
+Servo myservo2;  // 서보 모터 객체 생성
+int servoPin1 = 9;
+int servoPin2 = 10;  // 서보 모터가 연결된 핀 번호
+
+
+void setup() {
+  Serial.begin(9600); // 시리얼 통신 시작
+  myservo1.attach(servoPin1);
+  myservo2.attach(servoPin2);  // 서보 모터 핀 초기화
+}
+
+void loop() {
+  int sensorValue = 0;
+  int can_check = 0;
+  
+  for (int i = 0; i < 3; i++)
+  {
+    sensorValue = analogRead(photoresistorPin);
+    Serial.print("Sensor Value: ");
+    Serial.println(sensorValue);
+    if (sensorValue <= threshold)
+      {
+        can_check++;
+      }
+    delay(100);
+  }
+
+
+  if (can_check == 0) {
+    Serial.println("Not Can"); // 어두운 환경 감지 시 메시지 출력
+    moveNonCan();
+  } else {
+    Serial.println("Can");
+    moveCan();
+  }
+
+  delay(1000); // 1초 대기 후 다시 측정
+}
+
+
+void moveCan() {
+  for (int angle = 90; angle >= 0; angle -= 10) {  // 0도에서 180도까지 30도씩 증가하면서 반복
+    myservo1.write(angle);
+    myservo2.write(180 - angle);  // 서보 모터를 움직이는 함수 호출
+    delay(100);
+  }
+  delay(2000);
+  for (int angle = 0; angle <= 90; angle += 10) {  // 0도에서 180도까지 30도씩 증가하면서 반복
+    myservo1.write(angle);
+    myservo2.write(180 - angle);  // 서보 모터를 움직이는 함수 호출
+    delay(100);
+  }
+}
+
+
+void moveNonCan() {
+  for (int angle = 90; angle <= 180; angle += 10) {  // 0도에서 180도까지 30도씩 증가하면서 반복
+    myservo1.write(angle);
+    myservo2.write(180 - angle);  // 서보 모터를 움직이는 함수 호출
+    delay(100);
+  }
+  delay(2000);
+  for (int angle = 180; angle >= 90; angle -= 10) {  // 0도에서 180도까지 30도씩 증가하면서 반복
+    myservo1.write(angle);
+    myservo2.write(180 - angle);  // 서보 모터를 움직이는 함수 호출
+    delay(100);
+  }
+}
